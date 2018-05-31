965e57849b98e51feb0bb7877c7b1e67f1a7747b
Josue Vidal <vidalmaizena@hotmail.com>
2018-05-31 15:43:51 -0300

    starer

---
+++ knife.rb
@@ -0,0 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "josuevidal"
+client_key               "#{current_dir}/josuevidal.pem"
+validation_client_name   'vidalcorp-validator'
+validation_key           '#{current_dir}/vidalcorp-validator.pem'
+chef_server_url          "https://api.chef.io/organizations/vidalcorp"
+cookbook_path            ["#{current_dir}/../cookbooks"]
