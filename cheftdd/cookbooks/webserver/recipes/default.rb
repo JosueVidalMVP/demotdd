d3b3e2c9088ffe1da974d5cfe1637609a4b5933a
Josue Vidal <josue@vidal.com>
2018-05-31 19:51:58 -0300

    Azure

--- cheftdd/cookbooks/webserver/recipes/default.rb
+++ cheftdd/cookbooks/webserver/recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+powershell_script 'Install IIS' do
+    action :run
+    code 'add-windowsfeature Web-Server'
+end
+
+service 'w3svc' do
+    action [ :enable, :start ]
+end
+
+template 'c:\inetpub\wwwroot\Default.htm' do
+    source 'Default.htm.erb'
+    rights :read, 'Everyone'
+end
--- cheftdd/knife.rb
+++ cheftdd/knife.rb
@@ -8,9 +8,9 @@ client_key               "#{current_dir}/josuevidal.pem"
 validation_client_name   'vidalcorp-validator'
 validation_key           '#{current_dir}/vidalcorp-validator.pem'
 chef_server_url          "https://api.chef.io/organizations/vidalcorp"
-cookbook_path            ["#{current_dir}/cheftdd/cookbooks"]
+cookbook_path            ["#{current_dir}/cookbooks"]
 knife[:azure_publish_settings_file] = "Windows Azure MSDN - 5_30_2018, 12_46_02 - credentials.publishsettings"
 option :a96705c9-bf6c-4e61-924d-e9298305874b          
 option :2ceeca14-0c1f-4672-9dbc-38e864d760e7                  
 option :vidalmaizena@hotmail.com                 
-option :c9l3giaL9  
+option :c9l3giaL9       
