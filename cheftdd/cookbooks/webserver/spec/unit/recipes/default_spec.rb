6bf05c3ef55e6d9a842c1f0c75f0465f9ad4bd70
Josue Vidal <josue@vidal.com>
2018-06-01 02:37:09 -0300

    okl

--- cheftdd/cookbooks/webserver/spec/unit/recipes/default_spec.rb
+++ cheftdd/cookbooks/webserver/spec/unit/recipes/default_spec.rb
@@ -11,25 +11,17 @@ describe 'webserver::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012R2')
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'enables iis service with name W3WVC' do
+      expect(chef_run).to enable_service('iis').with(service_name: 'w3svc')
     end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'starts iis service with name W3WVC' do
+      expect(chef_run).to start_service('iis').with(service_name: 'w3svc')
     end
   end
 end
