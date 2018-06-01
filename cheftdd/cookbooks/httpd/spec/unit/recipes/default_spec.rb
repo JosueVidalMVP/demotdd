58d82ae8a6297ea7139629b64addeffd192379e2
Josue Vidal <josue@vidal.com>
2018-05-31 22:59:09 -0300

    ok

--- cheftdd/cookbooks/httpd/spec/unit/recipes/default_spec.rb
+++ cheftdd/cookbooks/httpd/spec/unit/recipes/default_spec.rb
@@ -4,6 +4,20 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+#  context 'When all attributes are default, on CentOS 7.4.1708' do
+#    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+#      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+#      runner.converge(described_recipe)
+#    end
+
+#    it 'converges successfully' do
+#      expect { chef_run }.to_not raise_error
+#    end
+#  end
+#end
+
 require 'spec_helper'
 
 describe 'httpd::default' do
@@ -19,19 +33,6 @@ describe 'httpd::default' do
     end
     it 'installs apache2 package' do
       expect(chef_run).to install_package('apache2')
+    end
   end
 end
-
-#  context 'When all attributes are default, on CentOS 7.4.1708' do
-#    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-#      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-#      runner.converge(described_recipe)
-#    end
-
-#    it 'converges successfully' do
-#      expect { chef_run }.to_not raise_error
-#    end
-#  end
-#end
