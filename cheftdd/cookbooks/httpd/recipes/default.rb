8a194e9382cc0bae084393d89e3101c8fe9f1dda
Josue Vidal <josue@vidal.com>
2018-07-11 19:10:11 -0300

    codigo

--- cheftdd/cookbooks/httpd/recipes/default.rb
+++ cheftdd/cookbooks/httpd/recipes/default.rb
@@ -3,9 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-package 'apache2' do
-  action :install
-end
+#package 'apache2' do
+#  action :install
+#end
 
 #service 'apache2' do
 #  action :start
